faf6c24f285edf58e0aa2ff67b86f6bcf52d62a7
Rahul K <rahulsachin07314@gmail.com>
2025-07-17 20:44:01 +0530

    commit

--- index.jsx
+++ index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Chips from './src/ChipInput/Chips'
 import Toggle from './src/react-interview-task/Toggle/toggle'
 import Filter from './src/react-interview-task/FilterReceipee/Filter'
+import AgeCalculator from './src/react-interview-task/Age-Calculator/AgeCalculator'
 
 const Index = () => {
   return (
     // <Chips/>
     // <Toggle/>
-    <Filter/>
+    // <Filter/>
+    <AgeCalculator/>
   )
 }
 
