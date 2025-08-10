d7225ffdaefece01a6c36b70b8509a9f96f83514
Rahul K <rahulsachin07314@gmail.com>
2025-08-10 15:46:09 +0530

    create read more button

--- index.jsx
+++ index.jsx
@@ -3,13 +3,20 @@ import Chips from './src/ChipInput/Chips'
 import Toggle from './src/react-interview-task/Toggle/toggle'
 import Filter from './src/react-interview-task/FilterReceipee/Filter'
 import AgeCalculator from './src/react-interview-task/Age-Calculator/AgeCalculator'
+import ReadMoreToggle from './src/react-interview-task/ReadMore/ReadMore'
+import ReadMore from './src/react-interview-task/ReadMore/Read'
 
 const Index = () => {
   return (
-    // <Chips/>
+    <>
+       {/* // <Chips/>
     // <Toggle/>
     // <Filter/>
-    <AgeCalculator/>
+    // <AgeCalculator/> */}
+    <ReadMoreToggle/>
+    <ReadMore/>
+    </>
+ 
   )
 }
 
